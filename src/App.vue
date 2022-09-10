8be4a77988fc84b4f7d5f6543b778a364e8894f9
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2022-09-10 14:35:58 -0300

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
     <div class="urna">
+
+      <TelaUrna 
+        :tela="tela"
+        :numeroVoto="numeroVoto"
+        :quantidadeNumeros="quantidadeNumeros"
+      />
+
       <TecladoUrna />
+
     </div>
   </div>
 </template>
@@ -9,11 +17,20 @@
 <script>
 import '@/assets/css/global.css'
 import TecladoUrna from '@/components/Teclado.vue';
+import TelaUrna from '@/components/Tela.vue';
 
 export default {
   name: 'App',
   components: {
-    TecladoUrna
+    TecladoUrna,
+    TelaUrna
+  },
+  data() {
+    return {
+      tela: 'vereador',
+      numeroVoto: '2',
+      quantidadeNumeros: 2
+    }
   }
 }
 </script>
@@ -35,5 +52,7 @@ export default {
   padding: 30px;
   border-radius: 5px;
   display: flex;
+  justify-content: space-between;
 }
+
 </style>
--- src/components/Tela.vue
+++ src/components/Tela.vue
@@ -1,6 +1,30 @@
 <template>
-    <div>
+    <div class="urna-tela">
+        <div v-if="tela != 'fim'" class="urna-tela-voto">
+            
+            <div class="urna-tela-voto-textos">
+                <div class="urna-tela-voto-titulo">Seu voto para:</div>
+                <div class="urna-tela-voto-tipo">{{ tela }}</div>
+                <div class="urna-tela-voto-numeros">
+                    Números
+                    <div class="urna-tela-voto-numero" v-for="(value, key) in numeroVoto.padEnd(quantidadeNumeros, ' ')" :key="key">
+                        {{ value }}
+                    </div>
+                </div>
+            </div>
+            
+            <div class="urna-tela-voto-imagem">
+
+            </div>
+            
+            <div class="urna-tela-voto-instrucoes">
+
+            </div>
         
+        </div>
+        <div v-if="tela == 'fim'" class="urna-tela-fim">
+            finalizacao
+        </div>
     </div>
 </template>
 
@@ -8,7 +32,12 @@
 
 <script>
 export default {
-    
+    name: 'TelaUrna',
+    props: {
+        tela: String, 
+        numeroVoto: String,
+        quantidadeNumeros: Number
+    }
 }
 </script>
 
@@ -16,5 +45,40 @@ export default {
 
 
 <style scoped>
+.urna-tela{
+    width: 55%;
+    height: 100%;
+    background-color: var(--ballot-box-screen-color);
+    border-radius: 5px;
+    border: 2px solid var(--light-border-color);
+    padding: 20px;
+    color: var(--dark-text-color);
+}
 
+.urna-tela-voto-titulo {
+  font-size: 20px;
+}
+
+.urna-tela-voto-tipo {
+  font-size: 30px;
+  font-weight: bold;
+  margin: 20px 0;
+  text-transform: uppercase;
+}
+
+.urna-tela-voto-numeros {
+    display: flex;
+    align-items: center;
+}
+
+.urna-tela-voto-numero {
+   width: 45px;
+   height: 55px;
+   border: 1px solid var(--dark-border-color); 
+   margin-left: 10px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   font-size: 30px;
+}
 </style>
